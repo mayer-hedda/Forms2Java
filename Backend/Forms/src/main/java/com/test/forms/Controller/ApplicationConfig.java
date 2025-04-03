16ad3856ef7a272fae7ee5f04c6dac2e16cafe36
Mayer Hedda <mayer.hedda@gmail.com>
2025-04-03 12:41:50 +0200

    hülye commentek törlése

--- Backend/Forms/src/main/java/com/test/forms/Controller/ApplicationConfig.java
+++ Backend/Forms/src/main/java/com/test/forms/Controller/ApplicationConfig.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.test.forms.Controller;
 
 import java.util.Set;
 import javax.ws.rs.core.Application;
 
-/**
- *
- * @author Mayer Hedda
- */
+
 @javax.ws.rs.ApplicationPath("webresources")
 public class ApplicationConfig extends Application {
 
